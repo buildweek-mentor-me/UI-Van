2d7f9fc10923858be5fde13ad89cf4ea0ee9310f
van jordan <vanjordan46@hotmail.com>
2019-04-16 15:21:28 -0700

    Changed about page...messing with the JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 
+class Person{
+  constructor(firstName,lastName,role,profile,github){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.role = role;
+    this.profile = profile;
+    this.github = github;
+  }
+}
+const nikita = new Person("Nikita", "Thomas", "Team Lead", "This is a some saying", "http://github.com"); 
+const dylan = new Person("Dylan", "Dislers" "Backend", "This is a some saying", "http://github.com");
+const guillaume = new Person("Guillaume", "Savy", "Front End", "This is a some saying", "http://github.com");
+const van = new Person("Van", "Jordan" "UI", "This is a some saying", "http://github.com");
+const steve = new Person("Steve", "Rollins", "UI", "This is a some saying", "http://github.com");
 
+const tabs = document.querySelector(".tabcontent");
+const tablinks = document.querySelector(".tablinks");
 
-    const names = Array.from(document.querySelectorAll('.name'));
-    for(let i=0;i<names.length;i++){
-        name.addEventListener('click', function(event){
-            event.target.style.display = 'block';
-        }
