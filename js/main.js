dbf43a46b5cd64dc87d444f0af1cd1168ed83aed
van jordan <vanjordan46@hotmail.com>
2019-04-17 10:19:23 -0700

    added github button to about us

--- js/main.js
+++ js/main.js
@@ -8,12 +8,58 @@ class Person{
     this.github = github;
   }
 }
-const nikita = new Person("Nikita", "Thomas", "Team Lead", "This is a some saying", "http://github.com"); 
-const dylan = new Person("Dylan", "Dislers" "Backend", "This is a some saying", "http://github.com");
-const guillaume = new Person("Guillaume", "Savy", "Front End", "This is a some saying", "http://github.com");
-const van = new Person("Van", "Jordan" "UI", "This is a some saying", "http://github.com");
-const steve = new Person("Steve", "Rollins", "UI", "This is a some saying", "http://github.com");
+const people = [];
+people[0] = new Person("Nikita", "Thomas", "Team Lead", "This is a some saying", "http://github.com"); 
+people[1] = new Person("Dylan", "Dislers", "Backend", "This is a some saying", "http://github.com");
+people[2] = new Person("Guillaume", "Savy", "Front End", "This is a some saying", "http://github.com");
+people[3] = new Person("Van", "Jordan", "UI", "This is a some saying", "http://github.com");
+people[4] = new Person("Steve", "Rollins", "UI", "This is a some saying", "http://github.com");
 
-const tabs = document.querySelector(".tabcontent");
-const tablinks = document.querySelector(".tablinks");
+const  tabcontent = document.querySelector('.tabcontent');
+const tablinks = document.querySelectorAll('.tablinks');
 
+const fullname = document.querySelector('.name');
+const role = document.querySelector('.role');
+const profile = document.querySelector('.profile');
+const git = document.querySelector('.github');
+
+
+tablinks.forEach(function(tablink){
+  people.forEach(function(person){
+    console.log(Object.values(person));
+    tablink.addEventListener('click', function(event){
+      console.log(event.target.classList[1]);
+      
+      if(event.target.classList[1] === person.firstName){
+        fullname.textContent = person.firstName + " " + person.lastName;
+        role.textContent = person.role;
+        profile.textContent = person.profile;
+        git.textContent= person.github;
+
+      }
+    });
+  });
+});
+
+//start event listeners for github buttons
+const van = document.querySelector('.van');
+const steve = document.querySelector('.steve');
+const nikita = document.querySelector('.nikita');
+const guillaume = document.querySelector('.guillaume');
+const dylan = document.querySelector('.dylan');
+
+van.addEventListener('click', function(event){
+location.href = "https://github.com/vanman9599";
+});
+steve.addEventListener('click', function(event){
+  location.href = "https://github.com/vanman9599";
+  });
+  nikita.addEventListener('click', function(event){
+    location.href = "https://github.com/vanman9599";
+    });
+    guillaume.addEventListener('click', function(event){
+      location.href = "https://github.com/vanman9599";
+      });
+      dylan.addEventListener('click', function(event){
+        location.href = "https://github.com/vanman9599";
+        });
